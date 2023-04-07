d66c4953ed3b641bd28bdf9566bf6c4fdf982e34
htchan <htchanbc@connect.ust.hk>
2023-04-07 10:28:47 +0800

    feat: add key binding ctrl+shift+up/down to control video speed

--- src/background.js
+++ src/background.js
@@ -1,14 +1,48 @@
+// default setting
+var setting = {
+  speed: 1,
+  mode: "default",
+  enable: false,
+  ignore: [],
+};
+
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.get('video_setting', (result) => {
-        if (!result.video_setting) {
-            chrome.storage.sync.set({
-                video_setting: {
-                    speed: 1,
-                    mode: 'default',
-                    enable: false,
-                    ignore: []
-                }
-            });
-        }
-    });
+  chrome.storage.sync.get("video_setting", (result) => {
+    if (!result.video_setting) {
+      chrome.storage.sync.set({
+        video_setting: setting,
+      });
+    }
+  });
+});
+
+chrome.storage.sync.get(
+  "video_setting",
+  ({ video_setting }) => (setting = video_setting)
+);
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  if (changes?.video_setting != null) {
+    setting = changes.video_setting.newValue;
+  }
+});
+
+chrome.commands.onCommand.addListener((command) => {
+  switch (command.toUpperCase()) {
+    case "FASTER":
+      setting.speed = (Number(setting.speed) + 0.25).toString();
+
+      chrome.storage.sync.set({
+        video_setting: setting,
+      });
+      break;
+    case "SLOWER":
+      setting.speed = (Number(setting.speed) - 0.25).toString();
+
+      chrome.storage.sync.set({
+        video_setting: setting,
+      });
+      break;
+    default:
+  }
 });
--- src/js/index.js
+++ src/js/index.js
@@ -1,56 +1,64 @@
-let enableButton = document.getElementById('enable');
-let speedInput = document.getElementById('speed');
-let modeDropdown = document.getElementById('mode');
-let ignoreDropdown = document.getElementById('ignore');
-var setting = {}
+let enableButton = document.getElementById("enable");
+let speedInput = document.getElementById("speed");
+let modeDropdown = document.getElementById("mode");
+let ignoreDropdown = document.getElementById("ignore");
+var setting = {};
 
-chrome.storage.sync.get('video_setting', ({video_setting}) => {
-    setting = video_setting;
+function applySetting(video_setting) {
+  setting = video_setting;
 
-    enableButton.checked = video_setting.enable;
+  enableButton.checked = setting.enable;
 
-    speedInput.value = video_setting.speed
+  speedInput.value = setting.speed;
 
-    Array.from(modeDropdown.children).forEach( (modeOption) => {
-        if (modeOption.getAttribute('value') == video_setting.mode) {
-            modeOption.selected = true;
-        }
-    });
-    
-    Array.from(ignoreDropdown.children).forEach( (ignoreOption) => {
-        if (video_setting.ignore.includes(ignoreOption.value)) {
-            ignoreOption.selected = true;
-        }
-    })
+  Array.from(modeDropdown.children).forEach((modeOption) => {
+    if (modeOption.getAttribute("value") == setting.mode) {
+      modeOption.selected = true;
+    }
+  });
+
+  Array.from(ignoreDropdown.children).forEach((ignoreOption) => {
+    if (setting.ignore.includes(ignoreOption.value)) {
+      ignoreOption.selected = true;
+    }
+  });
+}
+
+function updateSetting() {
+  chrome.storage.sync.set({
+    video_setting: setting,
+  });
+}
+
+chrome.storage.sync.get("video_setting", ({ video_setting }) => {
+  applySetting(video_setting);
 });
 
 enableButton.onclick = (event) => {
-    setting.enable = enableButton.checked;
-    chrome.storage.sync.set({
-        video_setting: setting
-    });
-}
+  setting.enable = enableButton.checked;
+  updateSetting();
+};
 
 speedInput.onchange = (event) => {
-    setting.speed = speedInput.value;
-    chrome.storage.sync.set({
-        video_setting: setting
-    });
-}
+  setting.speed = speedInput.value;
+  updateSetting();
+};
 
 modeDropdown.onchange = (event) => {
-    setting.mode = modeDropdown.value
-    chrome.storage.sync.set({
-        video_setting: setting
-    })
-}
+  setting.mode = modeDropdown.value;
+  updateSetting();
+};
 
 ignoreDropdown.onchange = (event) => {
-    setting.ignore = Array.from(ignoreDropdown.children)
-    .filter(item => item.selected)
-    .map(item => item.value);
+  setting.ignore = Array.from(ignoreDropdown.children)
+    .filter((item) => item.selected)
+    .map((item) => item.value);
 
-    chrome.storage.sync.set({
-        video_setting: setting
-    })
-}
+  updateSetting();
+};
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  if (changes.video_setting != null) {
+    applySetting(changes.video_setting.newValue);
+  }
+});
