78df95d19d84bcd023783129d66f3ad4c5388d6a
htchan <htchanbc@connect.ust.hk>
2023-04-07 10:28:15 +0800

    chore: run prettier

--- src/bilibili.js
+++ src/bilibili.js
@@ -1,111 +1,133 @@
-let _speedUp = (ratio = '2', trial = 0) => {
-    let player = document.getElementsByTagName("video")[0];
-    if (player.readyState < 1) {
-        if (trial < 10) { setTimeout(() => _speedUp(ratio, trial + 1), 1000); }
-    } else {
-        player.playbackRate = ratio;
+let _speedUp = (ratio = "2", trial = 0) => {
+  let player = document.getElementsByTagName("video")[0];
+  if (player.readyState < 1) {
+    if (trial < 10) {
+      setTimeout(() => _speedUp(ratio, trial + 1), 1000);
     }
-}
+  } else {
+    player.playbackRate = ratio;
+  }
+};
 let _changeView = (mode, trial = 0) => {
-    let isDefaultMode = document.getElementsByClassName("bpx-player-control-bottom-center")[0]?.childElementCount === 0;
-    let isTheaterMode = document.getElementsByClassName('bpx-player-ctrl-btn bpx-player-ctrl-web')[0]?.classList.contains('bpx-state-entered');
-    let isWideMode = document.getElementsByClassName('bpx-player-ctrl-btn bpx-player-ctrl-wide')[0]?.classList.contains('bpx-state-entered');
+  let isDefaultMode =
+    document.getElementsByClassName("bpx-player-control-bottom-center")[0]
+      ?.childElementCount === 0;
+  let isTheaterMode = document
+    .getElementsByClassName("bpx-player-ctrl-btn bpx-player-ctrl-web")[0]
+    ?.classList.contains("bpx-state-entered");
+  let isWideMode = document
+    .getElementsByClassName("bpx-player-ctrl-btn bpx-player-ctrl-wide")[0]
+    ?.classList.contains("bpx-state-entered");
 
-    let player = document.getElementsByTagName("video")[0];
-    if (player.readyState < 1) {
-        if (trial < 10) { setTimeout(() => _changeView(mode, trial + 1), 1000); }
+  let player = document.getElementsByTagName("video")[0];
+  if (player.readyState < 1) {
+    if (trial < 10) {
+      setTimeout(() => _changeView(mode, trial + 1), 1000);
     }
+  }
 
-    switch (mode?.toUpperCase()) {
-        case 'FULL SCREEN':
-            let fullScreenModeButton = document.getElementsByClassName('bpx-player-ctrl-btn bpx-player-ctrl-full')[0];
-            if (isDefaultMode || isTheaterMode || isWideMode) {
-                fullScreenModeButton.click();
-            }
-            break;
-        case 'THEATER':
-            let theaterModeButton = document.getElementsByClassName('bpx-player-ctrl-btn bpx-player-ctrl-web')[0]
-            if (!theaterModeButton && trial < 10) {
-                setTimeout(() => _changeView(mode, trial + 1), 1000);
-                return;
-            }
-            if (!isTheaterMode) {
-                theaterModeButton.click();
-            }
-            break;
-        default:
-            console.error(`unknown mode: ${mode}`);
-    }
-}
+  switch (mode?.toUpperCase()) {
+    case "FULL SCREEN":
+      let fullScreenModeButton = document.getElementsByClassName(
+        "bpx-player-ctrl-btn bpx-player-ctrl-full"
+      )[0];
+      if (isDefaultMode || isTheaterMode || isWideMode) {
+        fullScreenModeButton.click();
+      }
+      break;
+    case "THEATER":
+      let theaterModeButton = document.getElementsByClassName(
+        "bpx-player-ctrl-btn bpx-player-ctrl-web"
+      )[0];
+      if (!theaterModeButton && trial < 10) {
+        setTimeout(() => _changeView(mode, trial + 1), 1000);
+        return;
+      }
+      if (!isTheaterMode) {
+        theaterModeButton.click();
+      }
+      break;
+    default:
+      console.error(`unknown mode: ${mode}`);
+  }
+};
 
 let _play = (trial = 0) => {
-    let player = document.getElementsByTagName("video")[0];
-    if (player.readyState < 1) {
-        if (trial < 10) { setTimeout(() => _play(trial + 1), 1000); }
+  let player = document.getElementsByTagName("video")[0];
+  if (player.readyState < 1) {
+    if (trial < 10) {
+      setTimeout(() => _play(trial + 1), 1000);
     }
+  }
 
-    player.play();
-}
+  player.play();
+};
 
 let _pause = (trial = 0) => {
-    let player = document.getElementsByTagName("video")[0];
-    if (player.readyState < 1) {
-        if (trial < 10) { setTimeout(() => _pause(trial + 1), 1000); }
+  let player = document.getElementsByTagName("video")[0];
+  if (player.readyState < 1) {
+    if (trial < 10) {
+      setTimeout(() => _pause(trial + 1), 1000);
     }
+  }
 
-    player.pause();
-}
+  player.pause();
+};
 
 let _getTags = (trial = 0) => {
-    let tags = Array.from(document.getElementsByClassName('tag'))
-        .map(item => item.children[0].children[0].innerText);
-    return tags;
-}
+  let tags = Array.from(document.getElementsByClassName("tag")).map(
+    (item) => item.children[0].children[0].innerText
+  );
+  return tags;
+};
 
+let applySetting = ({ video_setting }) => {
+  if (!video_setting.enable) {
+    return;
+  }
 
-let applySetting = ({video_setting}) => {
-    if (!video_setting.enable) { return; }
+  let video = {
+    speedUp: _speedUp,
+    changeMode: _changeView,
+    control: {
+      play: _play,
+      pause: _pause,
+    },
+    // tags: _getTags()
+  };
 
-    let video = {
-        speedUp: _speedUp,
-        changeMode : _changeView,
-        control: {
-            play: _play,
-            pause: _pause,
-        },
-        // tags: _getTags()
-    }
+  let tags = document.getElementsByClassName("tag");
+  tags = Array.from(tags).map((tag) => tag.innerText.trim());
+  skip = tags.filter((tag) => video_setting.ignore.includes(tag));
+  if (skip.length != 0) {
+    return video;
+  }
 
-    let tags = document.getElementsByClassName('tag');
-    tags = Array.from(tags).map(tag => tag.innerText.trim());
-    skip = tags.filter( (tag) => video_setting.ignore.includes(tag));
-    if (skip.length != 0) { return video; }
+  console.log("start auto setup");
+  console.log("click play button");
 
-    console.log('start auto setup')
-    console.log('click play button')
+  console.log("change mode", video_setting.mode);
+  video.changeMode(video_setting.mode);
 
-    console.log('change mode', video_setting.mode);
-    video.changeMode(video_setting.mode);
-    
-    console.log('change speed', video_setting.speed)
-    video.speedUp(video_setting.speed)
+  console.log("change speed", video_setting.speed);
+  video.speedUp(video_setting.speed);
 
-    return video
-}
+  return video;
+};
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
-    console.log(changes)
-    console.log(changes['video_setting'] != null)
-    if (changes['video_setting'] != null) {
-        console.log(changes.video_setting.newValue)
-        applySetting({ video_setting: changes.video_setting.newValue })
-    }
-})
+  console.log(changes);
+  console.log(changes["video_setting"] != null);
+  if (changes["video_setting"] != null) {
+    console.log(changes.video_setting.newValue);
+    applySetting({ video_setting: changes.video_setting.newValue });
+  }
+});
 
 let player = document.getElementsByTagName("video")[0];
 player.onloadedmetadata = () => {
-    chrome.storage.sync.get('video_setting', (value) => {
-        video = applySetting(value);
-        // video.control.play();
-    });
+  chrome.storage.sync.get("video_setting", (value) => {
+    video = applySetting(value);
+    // video.control.play();
+  });
 };
--- src/youtube.js
+++ src/youtube.js
@@ -1,135 +1,168 @@
-let _speedUp = (ratio = '2', trial = 0) => {
-    setting_button = document.getElementsByClassName('ytp-button ytp-settings-button')[0];
+let _speedUp = (ratio = "2", trial = 0) => {
+  setting_button = document.getElementsByClassName(
+    "ytp-button ytp-settings-button"
+  )[0];
+  setting_button.click();
+
+  control_menu_buttons = Array.from(
+    document.getElementsByClassName("ytp-menuitem")
+  );
+  speed_control_button = control_menu_buttons.filter(
+    (row) => row.children[1].innerText === "Playback speed"
+  )[0];
+  if (!speed_control_button) {
     setting_button.click();
-
-    control_menu_buttons = Array.from(document.getElementsByClassName('ytp-menuitem'));
-    speed_control_button = control_menu_buttons.filter(
-        (row) => row.children[1].innerText === 'Playback speed'
-    )[0];
-    if (!speed_control_button) {
-        setting_button.click()
-        if (trial < 10) { setTimeout(() => _speedUp(ratio, trial + 1), 1000); }
-        return
+    if (trial < 10) {
+      setTimeout(() => _speedUp(ratio, trial + 1), 1000);
     }
-    speed_control_button.click();
+    return;
+  }
+  speed_control_button.click();
 
-    speed_options = Array.from(document.getElementsByClassName('ytp-menuitem-label'));
-    target_speed_option = speed_options.filter((option) => option.innerHTML == ratio || (ratio == 1 && option.innerHTML.toUpperCase() == "NORMAL"))?.[0];
-    console.log('speed option', target_speed_option)
-    if (target_speed_option) {
-        target_speed_option.click();
-    } else {
-        alert(ratio + ' not found');
-    }
-    setting_button.click()
-}
+  speed_options = Array.from(
+    document.getElementsByClassName("ytp-menuitem-label")
+  );
+  target_speed_option = speed_options.filter(
+    (option) =>
+      option.innerHTML == ratio ||
+      (ratio == 1 && option.innerHTML.toUpperCase() == "NORMAL")
+  )?.[0];
+  console.log("speed option", target_speed_option);
+  if (target_speed_option) {
+    target_speed_option.click();
+  } else {
+    alert(ratio + " not found");
+  }
+  setting_button.click();
+};
 
-var originalTrackCues = null
+var originalTrackCues = null;
 let _speedUpV2 = (ratio = 2) => {
-    let player = document.querySelector('video');
-    console.log(player?.readyState)
-    if (!player || player.readyState < 1) {
-        setTimeout(() => _speedUpV2(ratio), 1000)
-    } else {
-        let targetRatioV1 = 2
-        if (ratio < 2) { targetRatioV1 = ratio }
-        _speedUp(targetRatioV1)
-        player.playbackRate = ratio;
-
-        // speed up the subtitle
-        let trackList = document.querySelector('video').textTracks;
-        for (let i = 0; i < trackList.length; i++) {
-            let track = trackList[i];
-            track.mode = 'showing'; // set to 'hidden' if already showing so it will update
-            for (let j = 0; j < track.cues.length; j++) {
-                let cue = track.cues[j];
-                if (j == track.cues.length - 1) {
-                    console.log(cue)
-                    console.log(cue.startTime)
-                    console.log(cue.endTime)
-                }
-                cue.startTime /= ratio; // adjust the start time of the caption
-                cue.endTime /= ratio; // adjust the end time of the caption
-            }
-        }
+  let player = document.querySelector("video");
+  console.log(player?.readyState);
+  if (!player || player.readyState < 1) {
+    setTimeout(() => _speedUpV2(ratio), 1000);
+  } else {
+    let targetRatioV1 = 2;
+    if (ratio < 2) {
+      targetRatioV1 = ratio;
     }
-}
+    _speedUp(targetRatioV1);
+    player.playbackRate = ratio;
+
+    // speed up the subtitle
+    let trackList = document.querySelector("video").textTracks;
+    for (let i = 0; i < trackList.length; i++) {
+      let track = trackList[i];
+      track.mode = "showing"; // set to 'hidden' if already showing so it will update
+      for (let j = 0; j < track.cues.length; j++) {
+        let cue = track.cues[j];
+        if (j == track.cues.length - 1) {
+          console.log(cue);
+          console.log(cue.startTime);
+          console.log(cue.endTime);
+        }
+        cue.startTime /= ratio; // adjust the start time of the caption
+        cue.endTime /= ratio; // adjust the end time of the caption
+      }
+    }
+  }
+};
 let _skipAds = () => {
-    skip_ads_button = document.getElementsByClassName('ytp-ad-skip-button ytp-button')?.[0]
-    skip_ads_button?.click();
-    close_ads_buttons = Array.from(document.getElementsByClassName('ytp-ad-overlay-close-button'))
-    close_ads_buttons.map(button => button?.click());
-    _speedUpV2(global_video_settng.speed)
-}
+  skip_ads_button = document.getElementsByClassName(
+    "ytp-ad-skip-button ytp-button"
+  )?.[0];
+  skip_ads_button?.click();
+  close_ads_buttons = Array.from(
+    document.getElementsByClassName("ytp-ad-overlay-close-button")
+  );
+  close_ads_buttons.map((button) => button?.click());
+  _speedUpV2(global_video_settng.speed);
+};
 
 let _changeViewMode = (mode, trial = 0) => {
-    let full_screen_button = document.getElementsByClassName('ytp-fullscreen-button ytp-button')[0];
+  let full_screen_button = document.getElementsByClassName(
+    "ytp-fullscreen-button ytp-button"
+  )[0];
 
-    let get_into_full_mode = ((mode?.toUpperCase() === 'FULL') &&
-        (!full_screen_button.getAttribute('title').toUpperCase().includes('EXIT')));
-    let exit_full_mode = ((mode?.toUpperCase() !== 'FULL') &&
-        (full_screen_button.getAttribute('title').toUpperCase().includes('EXIT')));
+  let get_into_full_mode =
+    mode?.toUpperCase() === "FULL" &&
+    !full_screen_button.getAttribute("title").toUpperCase().includes("EXIT");
+  let exit_full_mode =
+    mode?.toUpperCase() !== "FULL" &&
+    full_screen_button.getAttribute("title").toUpperCase().includes("EXIT");
 
-    if (get_into_full_mode || exit_full_mode) {
-        full_screen_button.click();
-    }
+  if (get_into_full_mode || exit_full_mode) {
+    full_screen_button.click();
+  }
 
-    if (mode?.toUpperCase() === 'FULL') { return; }
+  if (mode?.toUpperCase() === "FULL") {
+    return;
+  }
 
-    let view_button = document.getElementsByClassName('ytp-size-button ytp-button')[0]
+  let view_button = document.getElementsByClassName(
+    "ytp-size-button ytp-button"
+  )[0];
 
-    if (view_button.getAttribute('title').toUpperCase().includes(mode?.toUpperCase())) {
-        view_button.click()
-    } else if (trial < 10) {
-        setTimeout(() => _changeViewMode(mode, trial + 1), 100);
-    }
-}
+  if (
+    view_button
+      .getAttribute("title")
+      .toUpperCase()
+      .includes(mode?.toUpperCase())
+  ) {
+    view_button.click();
+  } else if (trial < 10) {
+    setTimeout(() => _changeViewMode(mode, trial + 1), 100);
+  }
+};
 
 let _skipAdsAuto = () => {
-    const config = { childList: true, subtree: true };
-    var skipAdsObserver = new MutationObserver(_skipAds)
-    let adsNode = document.getElementsByClassName('video-ads ytp-ad-module')[0];
-    adsNode && skipAdsObserver.observe(adsNode, config);
-}
+  const config = { childList: true, subtree: true };
+  var skipAdsObserver = new MutationObserver(_skipAds);
+  let adsNode = document.getElementsByClassName("video-ads ytp-ad-module")[0];
+  adsNode && skipAdsObserver.observe(adsNode, config);
+};
 
-var global_video_settng = null
+var global_video_settng = null;
 
 let applySetting = ({ video_setting }) => {
-    global_video_settng = video_setting
-    console.log(video_setting)
-    if (!video_setting.enable) { return; }
+  global_video_settng = video_setting;
+  console.log(video_setting);
+  if (!video_setting.enable) {
+    return;
+  }
 
-    let video = {
-        speedUp: _speedUpV2,
-        skipAds: _skipAds,
-        changeMode: _changeViewMode,
-    }
+  let video = {
+    speedUp: _speedUpV2,
+    skipAds: _skipAds,
+    changeMode: _changeViewMode,
+  };
 
-    console.log("running extension")
-    console.log("attempt to skip ads")
-    video.skipAds();
-    console.log("skip ads observer")
-    _skipAdsAuto();
+  console.log("running extension");
+  console.log("attempt to skip ads");
+  video.skipAds();
+  console.log("skip ads observer");
+  _skipAdsAuto();
 
-    let videoType = Array.from(document.getElementsByTagName("meta"))
-        .filter(item => item.getAttribute("itemprop") == "genre")[0].getAttribute("content")
-    if (videoType != "Music") {
-        console.log("change mode", video_setting.mode)
-        video.changeMode(video_setting.mode);
+  let videoType = Array.from(document.getElementsByTagName("meta"))
+    .filter((item) => item.getAttribute("itemprop") == "genre")[0]
+    .getAttribute("content");
+  if (videoType != "Music") {
+    console.log("change mode", video_setting.mode);
+    video.changeMode(video_setting.mode);
 
-        console.log("change speed", video_setting.speed)
-        video.speedUp(video_setting.speed);
-    }
-}
+    console.log("change speed", video_setting.speed);
+    video.speedUp(video_setting.speed);
+  }
+};
 
-chrome.storage.sync.get('video_setting', applySetting)
-
+chrome.storage.sync.get("video_setting", applySetting);
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
-    console.log(changes)
-    console.log(changes['video_setting'] != null)
-    if (changes['video_setting'] != null) {
-        console.log(changes.video_setting.newValue)
-        applySetting({ video_setting: changes.video_setting.newValue })
-    }
-})
+  console.log(changes);
+  console.log(changes["video_setting"] != null);
+  if (changes["video_setting"] != null) {
+    console.log(changes.video_setting.newValue);
+    applySetting({ video_setting: changes.video_setting.newValue });
+  }
+});
